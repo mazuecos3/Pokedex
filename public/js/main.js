e94b2b7fec6aacaf4b30818c294e4743b325213d
mazuecos3 <oscar3mazuecos@hotmail.com>
2020-02-13 01:24:00 +0100

    Añadidas las imagenes y nombres, da igual el numero de pokemons.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 // Direccion json web
-const fuentesUrl = "https://pokeapi.co/api/v2/pokemon";
+const fuentesUrl = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20";
 // Simple script to use with datosAbiertos
 
 // Esta es la funcion de filtrado para
@@ -15,16 +15,14 @@ function filtroLetra(elemento) {
 // Pasa a mayuscula el texto de propio input
 // se lanza cada vez que se realiza una insercion en
 // el texto del nombre.
-function toUpp() {
+function toLowerCase() {
     document.querySelector(`input[name="nombrePokemon"]`).value = document.querySelector(`input[name="nombrePokemon"]`).value.toLowerCase();
 }
-
 
 function buscar() {
 
     // Obtenemos el JSON que esta definido
     const fetchPromesa = fetch(fuentesUrl);
-
     // Cuando se resuelva la promesa
     fetchPromesa.then(response => {
         // la pasamos a JSON
@@ -32,34 +30,56 @@ function buscar() {
         // Y entonces
     }).then(respuesta => {
         // Filtramos los resultados con el filtro definido anteriormente
-
-        console.log(respuesta.results);
-        const resultado = respuesta.results.filter(filtroLetra);
-
-        let divPokemons = document.createElement("div");
-
-
-        resultado.forEach(pokemon => {
-
-            let calleli = document.createElement("p");
-            calleli.innerHTML = pokemon.name;
-
-            divPokemons.appendChild(calleli);
-        });
-        // Establecemos el divPokemons en la Web
-        document.querySelector(".resultados").innerHTML = "";
-        document.querySelector(".resultados").appendChild(divPokemons);
+        let resultado = respuesta.results.filter(filtroLetra);
+        buscarPokemons(resultado);
     });
+}
+
+function buscarPokemons(resultado) {
+    let imagen;
+    let divPokemon;
+    let nombre;
+    let divPokemons = document.createElement("div");
+    divPokemons.id = "divPokemons";
+
+    //hacemos un foreach y luego un fetch para poder coger la url de cada pokemon
+    //tambien cogeremos el nombre y alguna características.
+    resultado.forEach(pokemon => {
+
+        const fetchPokemon = fetch(pokemon.url);
+        // console.log(pokemon.url);
+        fetchPokemon.then(response => {
+            // console.log(response);
+            return response.json();
+        }).then(respuesta => {
+
+            divPokemon = document.createElement("div");
+            divPokemon.id = "divPokemon";
+            //nos quedamos con la imagen frontal de cada pokemon
+            imagen = document.createElement("img");
+            imagen.src = respuesta.sprites.front_default;
+
+            //console.log(imagen);
+            nombre = document.createElement("p");
+            nombre.innerHTML = pokemon.name;
+
+            //creamos un div para cada pokemon para establecer imagen,texto,etc...
+            divPokemon.appendChild(imagen);
+            divPokemon.appendChild(nombre);
+            divPokemons.appendChild(divPokemon);
+        });
+    });
+    // Establecemos el divPokemons en la Web
+    document.querySelector(".resultados").innerHTML = "";
+    document.querySelector(".resultados").appendChild(divPokemons);
 }
 
 function init() {
 
-    // Binding de los eventos correspondientes.
-
     // Click en el boton de buscar
     document.querySelector(`input[type="button"]`).addEventListener("click", buscar);
     // Texto cambia en el <input>
-    document.querySelector(`input[type="text"]`).addEventListener("input", toUpp);
+    document.querySelector(`input[type="text"]`).addEventListener("input", toLowerCase);
 }
 
 // The mother of the lamb.
